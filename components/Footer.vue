c8c39880356ce71768a17541a5ca3ea6b0810b43
MohammedAyman2018 <emidobibo20@gmail.com>
2019-10-03 18:49:04 +0200

    Finish page one

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,27 +2,27 @@
   <footer class="bg-indigo-800 py-10">
     <div class="container mx-auto">
       <div class="flex flex-wrap">
-        <ul class="text-gray-500 w-full text-center mb-10  sm:text-center sm:w-1/2 lg:w-1/4">
+        <ul class="text-gray-500 w-full text-center mb-10 sm:text-center sm:w-1/2 lg:text-left lg:w-1/4">
           <li class="font-bold text-white text-2xl mb-4">Company</li>
           <li class="mb-2">Careers</li>
           <li class="mb-2">Volunteers</li>
           <li class="mb-2">Sustainability</li>
         </ul>
-        <ul class="text-gray-500 w-full text-center mb-10  sm:text-center sm:w-1/2 lg:w-1/4">
+        <ul class="text-gray-500 w-full text-center mb-10 sm:text-center sm:w-1/2 lg:text-left lg:w-1/4">
           <li class="font-bold text-white text-2xl mb-4">Our Events</li>
           <li class="mb-2">Web Summit - Rome</li>
           <li class="mb-2">Collision - Madrid</li>
           <li class="mb-2">RISE - Hong Kong</li>
           <li class="mb-2">MoneyConf - Bucharest</li>
         </ul>
-        <ul class="text-gray-500 w-full text-center mb-10  sm:text-center sm:w-1/2 lg:w-1/4">
+        <ul class="text-gray-500 w-full text-center mb-10 sm:text-center sm:w-1/2 lg:text-left lg:w-1/4">
           <li class="font-bold text-white text-2xl mb-4">Usefull Links</li>
           <li class="mb-2">Pre-register for 2019</li>
           <li class="mb-2">News</li>
           <li class="mb-2">Support centre</li>
           <li class="mb-2">Accessibility policy</li>
         </ul>
-        <ul class="text-gray-500 w-full text-center mb-10  sm:text-center sm:w-1/2 lg:w-1/4">
+        <ul class="text-gray-500 w-full text-center mb-10 sm:text-center sm:w-1/2 lg:text-left lg:w-1/4">
           <li class="font-bold text-white text-2xl mb-4">Contact</li>
           <li class="mb-2 flex">
             <i class="fas fa-map-marker-alt text-red-700 mr-2 mt-1"></i>
@@ -38,6 +38,14 @@
           </li>
         </ul>
       </div>
+    </div>
+      <hr class="border-pink-700">
+      <div class="container mx-auto">
+        <div class="flex flex-wrap">
+
+            <p class="w-full text-center text-gray-500 text-xs pt-5">Copyright ©2019 All rights reserved | This template is made by 
+            <a href="https://colorlib.com" target="_blank">Colorlib</a> | Coded by <a href="https://github.com/MohammedAyman2018" target="_blank">Mohammed Ayman</a>     </p>
+        </div>
     </div>
   </footer>
 </template>
